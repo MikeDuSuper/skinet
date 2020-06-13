e7413dfb0937488ae61c33d8b8559dee08f0d861
Mike Du <mikedu@Mikes-MacBook-Pro.local>
2020-06-13 00:27:27 -0700

    End of Section 12

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbComponent, BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,10 @@ export class ProductDetailsComponent implements OnInit {
 product: IProduct;
 
   constructor(private shopService: ShopService,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute,
+              private bcService: BreadcrumbService) {
+                this.bcService.set('@productDetails', '');
+               }
 
   ngOnInit() {
     this.loadProduct();
@@ -23,6 +27,7 @@ product: IProduct;
       +this.activatedRoute.snapshot.paramMap.get('id')
     ).subscribe(product => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
     });
